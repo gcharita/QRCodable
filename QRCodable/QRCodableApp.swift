fd90a0d06f39fb932f33e78d5a2c7ce560d44b8c
Giorgos Charitakis <chgiorgos13@gmail.com>
2025-11-01 17:38:28 +0200

    Initial commit

---
+++ QRCodable/QRCodableApp.swift
@@ -0,0 +1,17 @@
+//
+//  QRCodableApp.swift
+//  QRCodable
+//
+//  Created by Giorgos Charitakis on 1/11/25.
+//
+
+import SwiftUI
+
+@main
+struct QRCodableApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
